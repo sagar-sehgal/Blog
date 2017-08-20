796188f1bfff7074e7c395a1ba01cf6f95969f33
sagar-sehgal <sagar.r16@iiits.in>
2017-08-21 00:55:56 +0530

    added template for detailed blog as well as CSS for the site

--- blog/urls.py
+++ blog/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls import url,include
 from . import views
 urlpatterns = [
 	url(r'^$',views.post_list,name="post_list"),
+	url(r'^(?P<id>\d+)/$',views.post_detail,name="post_detail"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
+from django.http import HttpResponse
 from .models import Post
 # Create your views here.
 def post_list(request):
-	return render(request,"blog/post_list.html",{'all_posts':Post.objects.all()})
+	all_posts=Post.objects.all().order_by("created_date")
+	return render(request,"blog/post_list.html",{'all_posts':all_posts})
+def post_detail(request,id):
+	obj=get_object_or_404(Post,pk=id)
+	return render(request,"blog/post.html",{'post':obj})
